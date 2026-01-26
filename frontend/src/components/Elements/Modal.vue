567006d3f63f7fc9bcfb0c3fc4a975522b9d986b
Joshua M. Boniface <joshua@boniface.me>
2026-01-25 22:08:27 -0500

    Fix modal colour

--- frontend/src/components/Elements/Modal.vue
+++ frontend/src/components/Elements/Modal.vue
@@ -1,7 +1,7 @@
 <template>
  <div v-if="modal.visible" class="modal d-block mt-5 pt-5" tabindex="-1">
       <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content bg-dark text-white">
           <div class="modal-header">
             <h5 class="modal-title">{{modal.title}}</h5>
           </div>
